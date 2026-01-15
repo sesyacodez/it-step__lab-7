bc4963a6d1860f16e62044eab354c671febf43a7
Stefaniia Koldovska <62096135+sesyacodez@users.noreply.github.com>
2026-01-15 15:57:49 +0000

    added items

--- src/components/items.component/item.component/item.component.js
+++ src/components/items.component/item.component/item.component.js
@@ -1,6 +1,34 @@
-import './item.module.css'
+import classes from "./item.module.css";
 
-export const ItemComponent = () => {
-
-    return (<></>)
-}
+export const ItemComponent = ({ image, title, price, salePrice, currency }) => {
+  return (
+    <div className={classes.item}>
+      {salePrice && <span className={classes.saleBadge}>SALE</span>}
+      <div className={classes.imageWrapper}>
+        <img src={image} alt={title} className={classes.image} />
+      </div>
+      <div className={classes.info}>
+        <h3 className={classes.title}>{title}</h3>
+        <div className={classes.priceWrapper}>
+          {salePrice ? (
+            <>
+              <span className={classes.oldPrice}>
+                {currency}
+                {price}
+              </span>
+              <span className={classes.salePrice}>
+                {currency}
+                {salePrice}
+              </span>
+            </>
+          ) : (
+            <span className={classes.price}>
+              {currency}
+              {price}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/items.component/items.component.js
+++ src/components/items.component/items.component.js
@@ -1,8 +1,20 @@
-import classes from './items.module.css'
-import { itemsData } from './items.data.js'
-import { ItemComponent } from './item.component/item.component.js'
+import classes from "./items.module.css";
+import { itemsData } from "./items.data.js";
+import { ItemComponent } from "./item.component/item.component.js";
 
 export const ItemsComponent = () => {
-
-    return (<div className={classes.container}></div>)
-}
+  return (
+    <div className={classes.container}>
+      {itemsData.map((item, index) => (
+        <ItemComponent
+          key={index}
+          image={item.image}
+          title={item.title}
+          price={item.price}
+          salePrice={item.salePrice}
+          currency={item.currency}
+        />
+      ))}
+    </div>
+  );
+};
